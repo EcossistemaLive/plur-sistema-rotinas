7c70bf2b9f9102387d8406058cace72a077e0190
EcossistemaLive <ecossistemalive@gmail.com>
2026-06-25 21:50:01 -0300

    fix: Use HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useState } from 'react';
 import './index.css';
 import ReceptionDashboard from './components/ReceptionDashboard';
